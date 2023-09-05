0ad8a5c4925f95ef4d6ddc1dcd68b30847e5580e
MasFlai <Mateoramirez1405@gmail.com>
2023-09-05 18:42:40 -0500

    Actualización Inventario

---
+++ Assets/SelectorView.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectorView : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+
+    private RectTransform _rectTransform;
+    private GameObject _selected;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent <RectTransform>();
+    }
+
+    private void Update()
+    {
+        var selectedGameObject = EventSystem.current.currentSelectedGameObject;
+        _selected = (selectedGameObject == null) ? _selected : selectedGameObject;
+        var selected = EventSystem.current.currentSelectedGameObject;
+
+        if (selected == null) return;
+
+        //transform.position = selected.transform.position;
+        transform.position = Vector3.Lerp(transform.position, selected.transform.position, _speed * Time.deltaTime);
+
+        var otherRect = selected.GetComponent<RectTransform>();
+
+        var horizontalLerp = Mathf.Lerp(_rectTransform.rect.size.x, otherRect.rect.size.x, _speed * Time.deltaTime);
+        var verticalLerp = Mathf.Lerp(_rectTransform.rect.size.y, otherRect.rect.size.y, _speed * Time.deltaTime);
+
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, otherRect.rect.size.x);
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, otherRect.rect.size.y);
+    }
+}
